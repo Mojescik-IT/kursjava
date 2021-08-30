e237dbf8c76e5c143916379b4a79fa88bcccba09
Mojescik-IT <mojescik.it@gmail.com>
2021-08-30 18:54:08 +0200

    Add class - CustomScanner

---
+++ src/CustomScanner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CustomScanner {
+
+/*    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Wprowadź swoje imię: ");
+        String name = scanner.nextLine();
+        System.out.println("Cześć " + name + "!");
+    }*/
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę, podaj pierwszą liczbę: ");
+        int firstNumber = scanner.nextInt();
+        int result = firstNumber * firstNumber;
+        System.out.println("Kwadrat tej liczby to: " + result);
+    }
+
+
+
+}
