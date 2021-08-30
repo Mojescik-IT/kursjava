dbe85e86e8179311a8c13b579f97499c2a2af757
Mojescik-IT <mojescik.it@gmail.com>
2021-08-30 18:08:04 +0200

    Add class - Zmienne

---
+++ src/Zmienne.java
@@ -0,0 +1,20 @@
+//ZMIENNE - zarezerowane miejsce w pamięci z określonym typem danych
+public class Zmienne {
+
+    public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+        int resoult;
+
+        String name = "Przemek";
+        firstNumber = 1;
+        secondNumber = 2;
+        resoult = firstNumber + secondNumber;
+
+        name = "Kasia";
+        System.out.println(name);
+        System.out.println(resoult);
+
+    }
+
+}
