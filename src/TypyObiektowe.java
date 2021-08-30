88f94a7f912dac62e1db79d155efdb5201446082
Mojescik-IT <mojescik.it@gmail.com>
2021-08-30 17:53:00 +0200

    Add class - Typy obiektowe

---
+++ src/TypyObiektowe.java
@@ -0,0 +1,46 @@
+public class TypyObiektowe {
+
+    Byte  firstNumber = 127;
+    Short secondNumber = 32689;
+    Integer thirdNumber = 32768999;
+    Long fourthNumber = 2148000L;
+
+
+    Float fifthNumber = 4.99934F;
+    Double sixthNumber = 3.999999999999999D;
+
+
+    Boolean prawda = true;
+    Boolean fałsz = false;
+
+
+    Character letter = 'A';
+
+    String hello = "Hello Przemek";
+
+
+    public static void main(String[] args) {
+
+        Byte  firstNumber = 127;
+        Short secondNumber = 32689;
+        Integer thirdNumber = 32768999;
+        Long fourthNumber = 2148000L;
+
+        Float fifthNumber = 4.99934F;
+        Double sixthNumber = 3.999999999999999D;
+
+        Boolean prawda = true;
+        Boolean fałsz = false;
+
+        Character letter = 'A';
+        String hello = "Hello Przemek";
+
+
+        System.out.println(hello.charAt(0));
+
+
+
+    }
+
+
+}
