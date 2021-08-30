9cd4e6ee8a46227af31ddd71fb7e10546226c6f2
Mojescik-IT <mojescik.it@gmail.com>
2021-08-30 19:07:10 +0200

    Add class - Calculator - homework

---
+++ src/Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    //napisz program który poprosi użytkownika o dwie liczby, a nastepnie wykona na nich podstawowe operacje matematyczne
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("Pierwsza liczba to: " + firstNumber +"\n" +
+        "Druga liczba to: " + secondNumber);
+
+        System.out.println("==================================");
+
+        int addition = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        float division = firstNumber / secondNumber;
+        int mod = firstNumber % secondNumber; //4 mieści się w 6 = 1, r.2
+
+        System.out.println("Dodawanie: " + addition);
+        System.out.println("Odejmowanie: " + subtraction);
+        System.out.println("Mnożenie: " + multiplication);
+        System.out.println("Dzielenie: " + division);
+        System.out.println("Modulo: " + mod);
+
+
+    }
+
+
+}
