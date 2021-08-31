d5dfb76644edbfdd172f72feda543ff01ddb8273
Mojescik-IT <mojescik.it@gmail.com>
2021-08-31 18:50:21 +0200

    Add class - AgeChecker - homework

---
+++ src/AgeChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+//Zapytaj użytkownika o wiek. W przypadku gdy ma mniej niż 18 lat na ekranie wyświetl informację, że nie może kupić alkoholu.
+// Gdy więcej niż 18 to podziękuj za zakupy.
+public class AgeChecker {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swój wiek: ");
+        int age = scanner.nextInt();
+        System.out.println("Twój wiek to: " + age);
+        System.out.println("============================");
+
+        if (age < 18) {
+            System.out.println("Nie możesz kupić alkoholu. Masz poniżej 18 lat");
+        } else if (age >= 18) {
+            System.out.println("Możesz kupić alkohol. Masz " + age + " lat" + "\n" + "Dziękujemy za zakupy");
+        } else if (age >= 0) {
+            System.out.println("Podaj poprawny wiek!");
+        }
+
+
+
+
+    }
+
+}
