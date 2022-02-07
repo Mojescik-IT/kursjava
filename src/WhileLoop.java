1ad5cc9fda102fa6a15494776ba4a67e84c50cf4
Mojescik-IT <mojescik.it@gmail.com>
2022-02-07 09:17:04 +0100

    Add new class - WHILE Loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,14 @@
+//pętla jest wykonywana póki warunek jest TRUE
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int number = 0;
+
+        while (number < 20) { //warunek
+            System.out.println("Moja wartos to " + number + " i jestem mniejsza od 20");
+            number++;
+        }
+    }
+
+
+}
