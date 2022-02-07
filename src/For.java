2b5d4683698b0b806c788a97eefa3ca00f6aa023
Mojescik-IT <mojescik.it@gmail.com>
2022-02-07 09:06:34 +0100

    Add new class - Switch

---
+++ src/For.java
@@ -0,0 +1,31 @@
+// Umożliwia cykliczne wykonywanie ciągu instrukcji określoną liczbę razy
+public class For {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+        //i - specjalna zmienna sterująca
+        for (int i = 0; i < number; i = i + 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("=====================");
+
+        int secondNumber = 0;
+        for (int i = 100; i > secondNumber; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("====================");
+        //liczby podzielne przez 5
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+
+}
