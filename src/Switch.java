d492c567bdd1874ca4c203a559cac00fb221bf61
Mojescik-IT <mojescik.it@gmail.com>
2022-02-07 08:54:13 +0100

    Add new class - Switch

---
+++ src/Switch.java
@@ -0,0 +1,22 @@
+public class Switch {
+
+    public static void main(String[] args) {
+
+        String danie = "Losos";
+
+        switch (danie) {
+            case "Pizza":
+                System.out.println("Cena to 22 zl");
+                break;
+            case "Losos":
+                System.out.println("Cena to 30 zl");
+                break;
+            case "Frytki":
+                System.out.println("Cena to 9 zl");
+                break;
+            default:
+                System.out.println("Brak dania w karcie");
+        }
+    }
+
+}
